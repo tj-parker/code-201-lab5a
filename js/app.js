b2c51b4ca5ea7880e128e6bcf7211c578d00edf0
Timothy Parker <tlparker71@gmail.com>
2022-06-18 22:18:01 -0700

    add and mult function

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+//testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -50,11 +50,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let add = sum(a, b);
+  add = sum(add[0], c);
+  let mult =  multiply(a, b);
+  mult = multiply(mult[0], c);
+  return [add[0],
+    mult[0],
+    `${a} and ${b} and ${c} sum to ${add[0]}.`,
+    `The product of ${a} and ${b} and ${c} is ${mult[0]}.`];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
