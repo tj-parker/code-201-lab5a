cb89b872ea22a0012feb3d766b9964312208c69b
Timothy Parker <tlparker71@gmail.com>
2022-06-18 22:36:26 -0700

    arraymult function

--- js/app.js
+++ js/app.js
@@ -85,7 +85,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+//testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -101,11 +101,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  let arrayMult = sumAndMultiply(multArr[0], multArr[1], multArr[2]);
+  return [arrayMult[1], `The numbers ${multArr} have a product of ${arrayMult[1]}.`];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
